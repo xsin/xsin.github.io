b0f954a5bf0e8eb6f34e675eb2faf63009bcbaad
mamboer <mamboer@live.com>
2013-07-04 16:18:27 +0800

    add xdemo.yixun.detail

---
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -0,0 +1,9 @@
+{
+	"name": "易迅商详体验优化",//DEMO名称
+	"ename" : "ecd.yixun.detail", //DEMO的英文ID
+	"author" : "levinhuang", //DEMO的作者
+	"version": "1.0",//DEMO版本号，如配置文件有更新，务必修改版本号。
+	"css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"], //DEMO嵌入目标页面的CSS,如果没有,则为“[]”
+	"js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/xdemo.yixun.detail.js"], //DEMO嵌入目标页面的CSS,如果没有,则为“[]”
+	"matches": "http://*.51buy.com/*" //DEMO的作用域，先暂时只支持匹配域名，域名请用斜杆结尾。
+}
---
+++ assets/js/xdemo.yixun.detail.js
@@ -0,0 +1,6 @@
+J(function($,p,pub){
+	pub.id="demo";
+	pub._onLoad = function(){
+		alert(this.id);
+	};
+});
