f9eb836df20251e24872c247fcce552db7415f58
mamboer <mamboer@live.com>
2013-07-04 16:43:02 +0800

    go xdemo.yixun.detail

--- assets/js/xdemo.yixun.detail.cfg.js
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -1,6 +1,6 @@
 {
   "name": "易迅商详体验优化",
-  "version": "1.0",
+  "version": "1.0.1",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"],
   "js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/xdemo.yixun.detail.js"],
   "matches": "http://*.51buy.com/*",
