005d608a693a1f76bc231b2122daac0bf69b9097
mamboer <mamboer@live.com>
2013-09-01 13:37:32 +0800

    xdata v1.0.4

--- assets/js/xdemo.yixun.xdata.js
+++ assets/js/xdemo.yixun.xdata.js
@@ -2,15 +2,16 @@
 var J=function(e){return function(t){var n=typeof t;if(n==="undefined"){return J}if(n==="string"){return J[t]}if(n==="function"){var r={_:{}};t.call(J,e,r._,r);if(!r.id){alert("A J module require a public id property!");return}if(J[r.id]){alert('A J module with id "'+r.id+'" exists!');return}J[r.id]=r;r=null}}}(jQuery);(function(e){var t={},n={};t.onLoaded=function(){for(var e in J){if(e==="init"||e==="onLoad"){continue}e=J[e];if(t.isFunc(e._onLoad)){e._onLoad.call(e);delete e._onLoad}if(e._){t.loadSub(e._);delete e._}}};t.initEvents=function(n){e(document).ready(t.onLoaded)};t.isFunc=function(e){return e&&typeof e==="function"};t.initSub=function(e){for(var n in e){n=e[n];if(!n){continue}if(t.isFunc(n._init)){n._init.call(n);delete n._init}for(var r in n){r=n[r];if(!r)continue;if(t.isFunc(r._init)){r._init.call(r);delete r._init}}}};t.loadSub=function(e){for(var n in e){n=e[n];if(!n){continue}if(t.isFunc(n._onLoad)){n._onLoad.call(n);delete n._onLoad}for(var r in n){r=n[r];if(!r)continue;if(t.isFunc(r._onLoad)){r._onLoad.call(r);delete r._onLoad}}}};n.init=function(n){J.opts=t.opts=n=e.extend(n||{},J.opts||{});for(var r in J){if(r==="init"||r==="onLoad"){continue}r=J[r];if(t.isFunc(r._init)){r._init.call(r);delete r._init}if(r._){t.initSub(r._)}}t.initEvents()};n.onLoad=t.onLoaded;for(var r in n){if(!r){continue}J[r]=n[r]}})(window["jQuery"]);
 /* E J */
 
+/* S 菜单 */
 J(function($,p,pub){
-    pub.id="xdata";
+    pub.id="xdata_menu";
     var $body = $('body');
     //menu
     p.menu={
         tpl:'<div id="xdataMenu" class="xdata_menu">'+
                 '<ul>'+
-                    '<li><a href="javascript:;" class="xdata_lnk1" rel="1">热区图</a></li>'+
-                    '<li><a href="javascript:;" class="xdata_lnk2" rel="2">运营数据</a></li>'+
+                    '<li><a href="javascript:;" class="xdata_lnk1" rel="1">总热区图</a></li>'+
+                    '<li><a href="javascript:;" class="xdata_lnk2" rel="2">模块数据</a></li>'+
                 '</ul>'+
             '</div>',
         _init:function(){
@@ -25,7 +26,13 @@ J(function($,p,pub){
             });
         }
     };
+});
+/* E 菜单 */
 
+/* S 热区 */
+J(function($,p,pub){
+    pub.id="xdata_heatmap";
+    var $body = $('body');
     //heatmap
     p.heatmap = {
         isRender:false,
@@ -120,4 +127,34 @@ J(function($,p,pub){
     };
 
 });
+/* E 热区 */
+
+/* S 模块运营数据 */
+J(function($,p,pub){
+    pub.id="xdata_mods";
+    var $body = $('body');
+    //mods
+    p.mods={
+        isRender:false,
+        _init:function(){
+            $body.bind('onXDataMenuClick',function(e,d){
+                if(d.rel=="2"){
+                    if(!p.mods.isRender){
+                        p.mods.render();
+                    }
+                    p.mods.toggleDisplay();
+                }
+            });
+        },
+        render:function(){
+            alert('todo by joy');
+            
+        },
+        toggleDisplay:function(){
+
+        }
+    };
+});
+/* E 模块运营数据 */
+
 J.init();
