109df1a5f7337e163ebad0d96a5d80bd9af65839
mamboer <mamboer@live.com>
2013-09-01 11:49:55 +0800

    go

--- assets/js/xdemo.yixun.detail.cfg.js
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -3,7 +3,7 @@
   "version": "1.0.1",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"],
   "js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/xdemo.yixun.detail.js"],
-  "matches": ["http://*.51buy.com/*"],
+  "matches": ["http://*.yixun.com/*"],
   "ename": "ecd.yixun.detail",
   "author": "levinhuang",
   "descrition": "易迅商详体验优化"
--- assets/js/xdemo.yixun.xdata.cfg.js
+++ assets/js/xdemo.yixun.xdata.cfg.js
@@ -2,7 +2,7 @@
   "name": "易迅数据统计",
   "version": "1.0.0",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.xdata.css"],
-  "js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/xdemo.yixun.xdata.js"],
+  "js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/libs/heatmap/heatmap.js","http://oxox.io/assets/js/xdemo.yixun.xdata.js"],
   "matches": ["http://*.yixun.com/*"],
   "ename": "ecd.yixun.xdata",
   "author": "levinhuang",
