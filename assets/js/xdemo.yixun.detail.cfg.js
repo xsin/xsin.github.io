908b872cb3ab8bdc1e4d176b3c7bab089531f949
mamboer <mamboer@live.com>
2013-09-01 12:20:35 +0800

    go

--- assets/js/xdemo.yixun.detail.cfg.js
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -1,8 +1,8 @@
 {
   "name": "易迅商详体验优化",
-  "version": "1.0.1",
+  "version": "1.0.2",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"],
-  "js": ["http://oxox.io/assets/js/libs/j/1.0.2/j.core.js","http://oxox.io/assets/js/xdemo.yixun.detail.js"],
+  "js": ["http://oxox.io/assets/js/xdemo.yixun.detail.js"],
   "matches": ["http://*.yixun.com/*"],
   "ename": "ecd.yixun.detail",
   "author": "levinhuang",
