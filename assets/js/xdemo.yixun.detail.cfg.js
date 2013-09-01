a6a75358db8780f08563a8c927cfb72309c24841
mamboer <mamboer@live.com>
2013-09-01 12:22:14 +0800

    go

--- assets/js/xdemo.yixun.detail.cfg.js
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -1,9 +1,9 @@
 {
   "name": "易迅商详体验优化",
-  "version": "1.0.2",
+  "version": "1.0.3",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"],
   "js": ["http://oxox.io/assets/js/xdemo.yixun.detail.js"],
-  "matches": ["http://*.yixun.com/*"],
+  "matches": ["http://*item.yixun.com/*"],
   "ename": "ecd.yixun.detail",
   "author": "levinhuang",
   "descrition": "易迅商详体验优化"
--- assets/js/xdemo.yixun.xdata.cfg.js
+++ assets/js/xdemo.yixun.xdata.cfg.js
@@ -1,9 +1,9 @@
 {
   "name": "易迅数据统计",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.xdata.css"],
   "js": ["http://oxox.io/assets/js/libs/heatmap/heatmap.js","http://oxox.io/assets/js/xdemo.yixun.xdata.js"],
-  "matches": ["http://*.yixun.com/*"],
+  "matches": ["http://*yixun.com/*"],
   "ename": "ecd.yixun.xdata",
   "author": "levinhuang",
   "descrition": "易迅数据统计"
