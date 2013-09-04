4468abd8dc62a57e211964e98e16446f78b7cdfd
mamboer <mamboer@live.com>
2013-09-04 13:44:57 +0800

    add G.ABTest

--- assets/js/xdemo.yixun.detail.cfg.js
+++ assets/js/xdemo.yixun.detail.cfg.js
@@ -1,6 +1,6 @@
 {
   "name": "易迅商详体验优化",
-  "version": "1.0.3",
+  "version": "1.0.4",
   "css": ["http://oxox.io/assets/css/xdemo.yixun.detail.css"],
   "js": ["http://oxox.io/assets/js/xdemo.yixun.detail.js"],
   "matches": ["http://*item.yixun.com/*"],
