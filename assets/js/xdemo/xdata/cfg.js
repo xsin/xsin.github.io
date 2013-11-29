257ef85c640f06388c05b019923b66c23b8dcbd2
Levin <mamboer@live.com>
2013-11-29 13:07:58 +0800

    gg

--- assets/js/xdemo/xdata/cfg.js
+++ assets/js/xdemo/xdata/cfg.js
@@ -1,10 +1,10 @@
 {
-  "name": "易数据V2",
-  "version": "2.0.0",
+  "name": "易讯边栏",
+  "version": "1.0.0",
   "css": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/css/xdemo.yixun.xdata.css"],
   "js": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/bootup.js"],
   "matches": ["http://*.yixun.com/*"],
-  "ename": "ecd.yixun.xdata2",
+  "ename": "ecd.yixun.xbar",
   "author": "levinhuang",
-  "descrition": "易数据V2"
+  "descrition": "易讯边栏"
 }
