7a5c61acbf1d3b5d6074730a328d824659e9ce0b
Levin <mamboer@live.com>
2013-11-29 13:10:48 +0800

    gg

--- assets/js/xdemo/xdata/cfg.js
+++ assets/js/xdemo/xdata/cfg.js
@@ -1,6 +1,6 @@
 {
   "name": "易讯边栏",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "css": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/css/xdemo.yixun.xdata.css"],
   "js": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/bootup.js"],
   "matches": ["http://*.yixun.com/*"],
