c330a9d9f0c354369b6060b17753b8f9919add95
Levin <mamboer@live.com>
2013-11-29 14:02:28 +0800

    g

--- assets/js/xdemo/xdata/bootup.js
+++ assets/js/xdemo/xdata/bootup.js
@@ -1,8 +1,8 @@
 (function($,R,D){
 
 var isDebug = location.href.indexOf('debug')!=-1,
-    prefixXData = "https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/",
-    prefixXCoupon = "https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xcoupon/",
+    prefixXData = "https://rawgithub.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/",
+    prefixXCoupon = "https://rawgithub.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xcoupon/",
     t = new Date().getTime(),
     bizJs = (!isDebug)?[prefixXData+'all.debug.js?t='+t]:[
         prefixXData+"score.js?t="+t,
