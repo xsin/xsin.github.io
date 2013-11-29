9a2d4430b9c49789bfaec1132149117946e716da
Levin <mamboer@live.com>
2013-11-29 13:18:09 +0800

    g

--- assets/js/xdemo/xdata/cfg.js
+++ assets/js/xdemo/xdata/cfg.js
@@ -1,8 +1,8 @@
 {
   "name": "易讯边栏",
-  "version": "1.0.1",
-  "css": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/css/xdemo.yixun.xdata.css"],
-  "js": ["https://raw.github.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/bootup.js"],
+  "version": "1.0.2",
+  "css": ["https://rawgithub.com/oxox/oxox.github.io/xbar_v1/assets/css/xdemo.yixun.xdata.css"],
+  "js": ["https://rawgithub.com/oxox/oxox.github.io/xbar_v1/assets/js/xdemo/xdata/bootup.js"],
   "matches": ["http://*.yixun.com/*"],
   "ename": "ecd.yixun.xbar",
   "author": "levinhuang",
